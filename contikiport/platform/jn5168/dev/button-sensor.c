40536cbe752eee407d094ac5757faab881fd868a
Beshr Al Nahas <beshr@sics.se>
2014-02-13 17:11:14 +0000

    -Implementing button IRQ
    git-svn-id: https://svn.sics.se/rich@17 fcb72e6c-c17a-4c30-a33c-18bd3a0eb518

--- contikiport/platform/jn5168/dev/button-sensor.c
+++ contikiport/platform/jn5168/dev/button-sensor.c
@@ -1,8 +1,52 @@
-
+#include "contiki.h"
+#include "lib/sensors.h"
 #include "dev/button-sensor.h"
+#include <AppHardwareApi.h>
+#include "dev/button-sensor.h"
+#include "Button.h"
+#include "dev/leds.h"
+
+#ifdef BUTTONS_ENABELED_CONF
+#define BUTTONS_ENABELED BUTTONS_ENABELED_CONF
+#else
+#define BUTTONS_ENABELED 1
+#endif
+
+#define DEBUG 1
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
 
 const struct sensors_sensor button_sensor;
+static uint32_t volatile irq_val=0,
+                             val=0;
+#define BUTTON0_DIO (BUTTON_0_PIN)
+#define BUTTONS_DIO BUTTON0_DIO
 
+#define BUTTON0_INT E_AHI_DIO8_INT
+
+PROCESS(debounce_process, "button debounce");
+#define DEBOUNCE_TIME (CLOCK_SECOND/50)
+/*---------------------------------------------------------------------------*/
+void button_ISR(uint32 u32DeviceId,	uint32 u32ItemBitmap)
+{
+  if (BUTTONS_ENABELED) {
+//  	if(! (u32DeviceId & E_AHI_DEVICE_SYSCTRL && u32ItemBitmap & BUTTON0_INT)) {
+//  		return;
+//  	}
+  	/* announce the change */
+  	sensors_changed(&button_sensor);
+  	/* disable this IRQ while the button stops oscillating */
+		vAHI_DioInterruptEnable(0, BUTTON0_DIO);
+		//irq_val = u8ButtonReadRfd();
+		/* let the debounce process know about this to enable it again */
+		process_poll(&debounce_process);
+	  leds_on(LEDS_ALL);
+  }
+}
 /*---------------------------------------------------------------------------*/
 void
 button_press(void)
@@ -13,19 +57,92 @@ button_press(void)
 static int
 value(int type)
 {
-  return 0;
+	switch (type & BUTTONS_ENABELED) {
+	case BUTTON_0_MASK:
+		return val & BUTTON_0_MASK;
+//	case BUTTON_1_MASK:
+//		return val & BUTTON_1_MASK;
+	}
+	return 0;
 }
 /*---------------------------------------------------------------------------*/
 static int
 configure(int type, int value)
 {
-  return 0;
+	if (BUTTONS_ENABELED) {
+		switch (type) {
+		case SENSORS_HW_INIT:
+			vButtonInitRfd();
+			//vAHI_DioSetDirection(BUTTONS_DIO, 0x00);
+			vAHI_SysCtrlRegisterCallback(button_ISR);
+			PRINTF("SENSORS_HW_INIT\n");
+			//vAHI_DioInterruptEdge(uint32 u32Rising,	uint32 u32Falling);
+			val = u8ButtonReadRfd();
+	    //val = (u32AHI_DioReadInput() & BUTTONS_DIO);
+			return 1;
+		case SENSORS_ACTIVE:
+			PRINTF("SENSORS_ACTIVE %u\n", value);
+			if (!value) {
+				//deactivate
+				vAHI_DioInterruptEnable(0, BUTTON0_DIO);
+			} else {
+				//activate
+				vButtonInitRfd();
+							//vAHI_DioSetDirection(BUTTONS_DIO, 0x00);
+							vAHI_SysCtrlRegisterCallback(button_ISR);
+							PRINTF("SENSORS_HW_INIT\n");
+							//vAHI_DioInterruptEdge(uint32 u32Rising,	uint32 u32Falling);
+							val = u8ButtonReadRfd();
+					    //val = (u32AHI_DioReadInput() & BUTTONS_DIO);
+
+				vAHI_DioInterruptEnable(BUTTON0_DIO, 0);
+			}
+			break;
+		case SENSORS_READY:
+			PRINTF("SENSORS_READY\n");
+			return 1;
+		default:
+			return 0;
+		}
+	}
+	return 1;
 }
 /*---------------------------------------------------------------------------*/
 static int
 status(int type)
 {
-  return 0;
+  return u8ButtonReadRfd() & type;
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(debounce_process, ev, data)
+{
+  static struct etimer et;
+
+	if (BUTTONS_ENABELED) {
+		PROCESS_BEGIN();
+		PRINTF("debounce_process begin\n");
+
+		while (1) {
+			PROCESS_YIELD_UNTIL(ev==PROCESS_EVENT_POLL || ev==PROCESS_EVENT_TIMER);
+
+			if(ev==PROCESS_EVENT_TIMER) {
+//				if (val != irq_val) {
+//					PRINTF("val,irq_val=%d,%d\n",val,irq_val);
+//					val = irq_val;
+//					sensors_changed(&button_sensor);
+//				}
+				PRINTF("debounce_process reactivate IRQ\n");
+				//reactivate IRQ
+				vAHI_DioInterruptEnable(BUTTON0_DIO, 0);
+			  leds_off(LEDS_ALL);
+			} else if(ev==PROCESS_EVENT_POLL){
+				etimer_set(&et, DEBOUNCE_TIME);
+				PRINTF("debounce_process set timer to reactivate IRQ\n");
+			}
+		}
+		PROCESS_END();
+	}
+	return 0;
 }
 /*---------------------------------------------------------------------------*/
 SENSORS_SENSOR(button_sensor, BUTTON_SENSOR,
