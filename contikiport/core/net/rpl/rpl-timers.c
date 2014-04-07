3ff5fc920c7c74226c4d0a4720f41b0256508eb8
Beshr Al Nahas <beshr@sics.se>
2014-04-07 15:56:55 +0000

    Enabling RPL annotation and statistics (for cooja)
    git-svn-id: https://svn.sics.se/rich@66 fcb72e6c-c17a-4c30-a33c-18bd3a0eb518

--- contikiport/core/net/rpl/rpl-timers.c
+++ contikiport/core/net/rpl/rpl-timers.c
@@ -46,7 +46,7 @@
 
 #if UIP_CONF_IPV6
 
-#define DEBUG DEBUG_NONE
+#define DEBUG DEBUG_ANNOTATE
 #include "net/uip-debug.h"
 
 /*---------------------------------------------------------------------------*/
