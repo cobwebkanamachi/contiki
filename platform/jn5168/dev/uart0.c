53ac5b83e97ad8782110f15dd0b31cbbf272c713
beshrns <beshr.ns@gmail.com>
2014-04-23 17:27:26 +0200

    fixing uart driver to take input for SLIP to work (input data IRQ was not enabled)

--- platform/jn5168/dev/uart0.c
+++ platform/jn5168/dev/uart0.c
@@ -60,7 +60,6 @@ static void
 vUartISR(uint32 u32DeviceId, uint32 u32ItemBitmap)
 {
 	static uint8 u8UartL;
-//	static uint32 u32Bytes;
 
 	switch(u32DeviceId){
 	case E_AHI_DEVICE_UART0:
@@ -74,17 +73,12 @@ vUartISR(uint32 u32DeviceId, uint32 u32ItemBitmap)
 	}
 
 	if (u32ItemBitmap == E_AHI_UART_INT_RXDATA || u32ItemBitmap == E_AHI_UART_INT_TIMEOUT) {
-		if (uart0_input)
+		if (uart0_input) {
 			uart0_input(u8AHI_UartReadData(u8UartL));
-		else
+		} else {
 			u8AHI_UartReadData(u8UartL);
+		}
 	} else if (u32ItemBitmap == E_AHI_UART_INT_TX) {
-//		u32Bytes = 0;
-		/*
-		 * if there is data in buffer waiting for tx and we've not filled the
-		 * hardware fifo up
-		 */
-//		while (u32Bytes++<TXBUFSIZE && ringbuf_elements(&txbuf)) {
 		if(ringbuf_elements(&txbuf)) {
 			transmitting = 1;
 			vAHI_UartWriteData(u8Uart, ringbuf_get(&txbuf)); /* write one byte to the UART */
@@ -107,73 +101,38 @@ uart0_writeb(unsigned char c)
 	/* push into ringbuf until there is space */
 	while (!ringbuf_put(&txbuf, c));
 	UART_vPTSChar(ringbuf_get(&txbuf));
-//	while(u16AHI_UartReadTxFifoLevel(u8Uart) >= TXBUFSIZE -1);
-//	vAHI_UartWriteData(u8Uart, c);
-	//watchdog_periodic();
-	/* push into ringbuf until there is space */
-//	while (!ringbuf_put(&txbuf, c));
-//	/*
-//	 * if there is already a tx in progress, we can expect a TX interrupt
-//	 * some time in the future, in which case the data we wrote to the tx
-//	 * buffer will get sent in due course to the UART in the interrupt
-//	 * service routine.
-//	 * if there is no tx in progress, there won't be a tx interrupt, and the
-//	 * byte won't get read from the buffer in the ISR, so we must write it
-//	 * to the UART tx FIFO here.
-//	 */
-//	if ( !transmitting && (u8AHI_UartReadLineStatus(u8Uart)
-//			& (E_AHI_UART_LS_THRE | E_AHI_UART_LS_TEMT)) == (E_AHI_UART_LS_THRE | E_AHI_UART_LS_TEMT)) {
-//			transmitting = 1;
-//			vAHI_UartWriteData(u8Uart, ringbuf_get(&txbuf));
-//			transmitting = 0;
-//	}
 }
 
 void
 uart0_init(uint8_t br)
 {
-//	transmitting = 0;
+	transmitting = 0;
 	ringbuf_init(&txbuf, txbuf_data, TXBUFSIZE);
-//	//ringbuf_init(&rxbuf, rxbuf_data, sizeof(rxbuf_data));
-//
-//	/* Disable use of CTS/RTS */
-//	vAHI_UartSetRTSCTS(E_AHI_UART_0, false);
-//	/* Enable UART */
-//	//vAHI_UartEnable(u8Uart);
-//	bAHI_UartEnable(u8Uart, txbuf_data, TXBUFSIZE, NULL, 0);
-//	vAHI_UartReset(u8Uart, TRUE, TRUE);
-//	vAHI_UartSetAutoFlowCtrl(u8Uart, E_AHI_UART_FIFO_LEVEL_14, true, true, true);
-//	/* Set settings */
-//	vAHI_UartSetControl( u8Uart, E_AHI_UART_EVEN_PARITY,
-//			E_AHI_UART_PARITY_DISABLE, E_AHI_UART_WORD_LEN_8, E_AHI_UART_1_STOP_BIT,
-//			E_AHI_UART_RTS_HIGH);
-//	/* Set baud rate */
-//	vAHI_UartSetClockDivisor(u8Uart, br);
-//
-//	/* install interrupt service callback */
-//#if u8Uart == E_AHI_UART_0
-//	vAHI_Uart0RegisterCallback((void*) vUartISR);
-//#else
-//	vAHI_Uart1RegisterCallback((void*)vUartISR);
-//#endif
-//	/* Enable TX Fifo empty and Rx data interrupts */
-//	vAHI_UartSetInterrupt(u8Uart, FALSE, /* modem status         */
-//	false, /* rx line error status */
-//	false, /* tx fifo empty        */
-//	false, /* rx data there        */
-//	E_AHI_UART_FIFO_LEVEL_14);
-//
-//	vAHI_UartReset(u8Uart, false, false);
-//	/* Note we opened the UART */
-//	bOpen = true;
 	/* Disable use of CTS/RTS */
 	vAHI_UartSetRTSCTS(u8Uart, FALSE);
 	/* Enable UART */
 	vAHI_UartEnable(u8Uart);
+
+	vAHI_UartReset(u8Uart, true, true);
+
 	/* Set settings */
 	vAHI_UartSetControl(u8Uart, E_AHI_UART_EVEN_PARITY, E_AHI_UART_PARITY_DISABLE, E_AHI_UART_WORD_LEN_8, E_AHI_UART_1_STOP_BIT, E_AHI_UART_RTS_HIGH);
+
+	/* Set interrupts */
+	vAHI_Uart0RegisterCallback(vUartISR);
+
+	vAHI_UartSetInterrupt(u8Uart, false,  /* modem status         */
+															false,  /* rx line error status */
+															true,   /* tx fifo empty        */
+															true,   /* rx data there        */
+															E_AHI_UART_FIFO_LEVEL_1);
+
 	/* Set baud rate */
 	vAHI_UartSetClockDivisor(u8Uart, br);
+
+	vAHI_UartSetRTSCTS(u8Uart, false);
+	vAHI_UartReset(u8Uart, false, false);
+
 	/* Note we opened the UART */
 	bOpen = TRUE;
 }
