e5658066f6c2709b678b793e91ac4b605969833b
Simon Duquennoy <simonduq@sics.se>
2014-07-31 18:26:07 +0200

    Now with 4 examples in tsch-testbed, 2 no-rpl, 2 rpl

---
+++ examples/tsch-testbed/app-no-rpl-unicast.c
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2013, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+  *
+ */
+/**
+ * \file
+ *         Example file using RPL for a any-to-any routing: the root and all
+ *         nodes with an even node-id send a ping periodically to another node
+ *         (which also must be root or have even node-id). Upon receiving a ping,
+ *         nodes answer with a poing.
+ *         Can be deployed in the Indriya or Twist testbeds.
+ *
+ * \author Simon Duquennoy <simonduq@sics.se>
+ */
+
+#include "contiki-conf.h"
+#include "net/netstack.h"
+#include "net/uip-ds6-nbr.h"
+#include "lib/random.h"
+#include "deployment.h"
+#include "simple-energest.h"
+#include "simple-udp.h"
+#include "cc2420.h"
+#include <stdio.h>
+
+#define SEND_INTERVAL   (60 * CLOCK_SECOND)
+#define UDP_PORT 1234
+
+static struct simple_udp_connection unicast_connection;
+static uint16_t current_cnt = 0;
+static uip_ipaddr_t llprefix;
+
+#define DEST_ID 2
+#define SRC_ID 3
+
+/*---------------------------------------------------------------------------*/
+PROCESS(unicast_sender_process, "No-RPL Unicast Application");
+AUTOSTART_PROCESSES(&unicast_sender_process);
+/*---------------------------------------------------------------------------*/
+void app_send_to(uint16_t id, int ping, uint32_t seqno);
+static void
+receiver(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *dataptr,
+         uint16_t datalen)
+{
+  struct app_data data;
+  appdata_copy(&data, (struct app_data*)dataptr);
+  if(data.ping) {
+    RPL_LOG_FROM_APPDATAPTR((struct app_data *)dataptr, "App: received ping");
+  } else {
+    RPL_LOG_FROM_APPDATAPTR((struct app_data *)dataptr, "App: received pong");
+  }
+  if(data.ping) {
+    app_send_to(data.src, 0, data.seqno | 0x8000l);
+  }
+}
+/*---------------------------------------------------------------------------*/
+void
+app_send_to(uint16_t id, int ping, uint32_t seqno)
+{
+  struct app_data data;
+  uip_ipaddr_t dest_ipaddr;
+  uip_lladdr_t dest_lladdr;
+
+  data.seqno = seqno;
+  data.src = node_id;
+  data.dest = id;
+  data.hop = 0;
+  data.fpcount = 0;
+  data.ping = ping;
+
+  if(ping) {
+    RPL_LOG_FROM_APPDATAPTR(&data, "App: sending ping");
+  } else {
+    RPL_LOG_FROM_APPDATAPTR(&data, "App: sending pong");
+  }
+
+  set_ipaddr_from_id(&dest_ipaddr, id);
+  /* Convert global address into link-local */
+  memcpy(&dest_ipaddr, &llprefix, 8);
+  set_rimeaddr_from_id(&dest_lladdr, id);
+	uip_ds6_nbr_add(&dest_ipaddr, &dest_lladdr, 1, ADDR_MANUAL);
+
+  simple_udp_sendto(&unicast_connection, &data, sizeof(data), &dest_ipaddr);
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(unicast_sender_process, ev, data)
+{
+  static struct etimer periodic_timer;
+  static struct etimer send_timer;
+  uip_ipaddr_t global_ipaddr;
+
+  PROCESS_BEGIN();
+
+  if(node_id == 0) {
+    NETSTACK_RDC.off(0);
+    printf("Node id unset, my mac is ");
+    uip_debug_lladdr_print(&rimeaddr_node_addr);
+    printf("\n");
+    PROCESS_EXIT();
+  }
+
+  uip_ip6addr(&llprefix, 0xfe80, 0, 0, 0, 0, 0, 0, 0);
+  cc2420_set_txpower(RF_POWER);
+  cc2420_set_cca_threshold(RSSI_THR);
+  simple_energest_start();
+
+  printf("App: %u starting\n", node_id);
+
+  deployment_init(&global_ipaddr);
+  simple_udp_register(&unicast_connection, UDP_PORT,
+                      NULL, UDP_PORT, receiver);
+
+  if(node_id == SRC_ID) {
+    etimer_set(&periodic_timer, SEND_INTERVAL);
+    while(1) {
+    	app_send_to(DEST_ID, 1, ((uint32_t)node_id << 16) + current_cnt);
+    	current_cnt++;
+
+      PROCESS_WAIT_UNTIL(etimer_expired(&periodic_timer));
+      etimer_reset(&periodic_timer);
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
