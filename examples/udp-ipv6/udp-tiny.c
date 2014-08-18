6cbd539636331128139f8431d3b6b09b86817de9
beshrns <beshr.ns@gmail.com>
2014-08-18 11:06:24 +0200

    Old examples commited

---
+++ examples/udp-ipv6/udp-tiny.c
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2011, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+#include "contiki.h"
+#include "lib/random.h"
+#include "sys/ctimer.h"
+#include "sys/etimer.h"
+#include "net/uip.h"
+#include "net/uip-ds6.h"
+
+#include "simple-udp.h"
+
+#include "leds.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#define UDP_PORT 4000
+#define REMOTE_UDP_PORT 4001
+
+#define SEND_INTERVAL		(CLOCK_SECOND/2)
+#define SEND_TIME		(/*random_rand() %*/ (SEND_INTERVAL))
+
+static struct simple_udp_connection broadcast_connection;
+static uip_ipaddr_t server_ipaddr;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(broadcast_example_process, "UDP broadcast example process");
+AUTOSTART_PROCESSES(&broadcast_example_process);
+/*---------------------------------------------------------------------------*/
+static void
+receiver(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+  printf("Data %d received on port %d from port %d with length %d\n",
+         data[0], receiver_port, sender_port, datalen);
+  leds_arch_set(data[0]);
+}
+/*---------------------------------------------------------------------------*/
+static void
+set_global_address(void)
+{
+  uip_ipaddr_t ipaddr;
+
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+/* The choice of server address determines its 6LoPAN header compression.
+ * (Our address will be compressed Mode 3 since it is derived from our link-local address)
+ * Obviously the choice made here must also be selected in udp-server.c.
+ *
+ * For correct Wireshark decoding using a sniffer, add the /64 prefix to the 6LowPAN protocol preferences,
+ * e.g. set Context 0 to aaaa::.  At present Wireshark copies Context/128 and then overwrites it.
+ * (Setting Context 0 to aaaa::1111:2222:3333:4444 will report a 16 bit compressed address of aaaa::1111:22ff:fe33:xxxx)
+ *
+ * Note the IPCMV6 checksum verification depends on the correct uncompressed addresses.
+ */
+
+#if 1
+/* Mode 1 - 64 bits inline */
+   uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
+#elif 0
+/* Mode 2 - 16 bits inline */
+  uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
+#else
+/* Mode 3 - derived from server link-local (MAC) address */
+  uip_ip6addr(&server_ipaddr, 0xaaaa, 0, 0, 0, 0x0250, 0xc2ff, 0xfea8, 0xcd1a); //redbee-econotag
+#endif
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(broadcast_example_process, ev, data)
+{
+  static struct etimer periodic_timer;
+  static struct etimer send_timer;
+  static uint8_t i =0;
+  uip_ipaddr_t addr;
+
+  PROCESS_BEGIN();
+  leds_init();
+  int
+  cc2420_set_channel(int c);
+
+  cc2420_set_channel(CC2420_CONF_CHANNEL);
+  set_global_address();
+
+  simple_udp_register(&broadcast_connection, UDP_PORT,
+                      NULL, REMOTE_UDP_PORT,
+                      receiver);
+
+  etimer_set(&periodic_timer, SEND_INTERVAL);
+  while(1) {
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    etimer_reset(&periodic_timer);
+//    etimer_set(&send_timer, SEND_TIME);
+//
+//    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&send_timer));
+    printf("Sending broadcast\n");
+    uip_create_linklocal_allnodes_mcast(&addr);
+    i++;
+    simple_udp_sendto(&broadcast_connection, &i, 1, &addr);
+    leds_blink();
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
